acbb056de78b83f6db9b4efadb04fc1bca8ee2ce
Cheng Fang <cfang@redhat.com>
2018-08-14 21:36:06 -0400

    change main-class to org.jberet.samples.simple.Main

---
+++ src/main/java/org/jberet/samples/simple/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package org.jberet.samples.simple;
+
+public class Main {
+    public static void main(String[] args) {
+        org.jberet.se.Main.main(args);
+    }
+}
